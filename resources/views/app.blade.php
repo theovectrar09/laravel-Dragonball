fbe81e41a2d86a6a5e1f31d220e373f75da8a586
theovectrar09 <vectrar.theo@gmail.com>
2019-02-11 00:52:02 +0700

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="{!! asset('css/style.css') !!}" media="screen">
+	<title>TEAM</title>
+</head>
+<body style="margin:0">
+	<div class="images">
+		<img src="images/team.png" alt="" style="margin-left:auto;margin-right:auto;display:block;width:50%;">
+		{{-- <center><h1 style="font-family:'Nunito', sans-serif;color: #636b6f;font-weight: 200;height: 100vh;margin: 0;">DRAGON BALL</h1></center> --}}
+	</div>
+	<img src="images/api.gif" alt="Api" style="width:100%">
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 20px;
+                font-size: 15px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                    {{-- <img src="images/SonGoku.png" alt="HTML5 Icon" style="width:128px;height:128px;"> --}}
+                <div class="title m-b-md" style="background-image: url('images/SonGoku.png');background-size:100px;background-position:center;background-repeat:no-repeat">
+                    Theo Vectra Riyadi
+                </div>
+
+                <div class="links">
+                    <a href="{{ URL('/app') }}" class="btn btn-sm btn-raised btn-info">NEXT</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
