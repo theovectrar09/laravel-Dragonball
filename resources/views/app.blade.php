8972f7beaba8e32f4d2f6f9b538b7b98087b8c1c
theovectrar09 <vectrar.theo@gmail.com>
2019-02-12 16:12:42 +0700

    kedua

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,13 +5,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="{!! asset('css/style.css') !!}" media="screen">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<title>TEAM</title>
 </head>
-<body style="margin:0">
-	<div class="images">
-		<img src="images/team.png" alt="" style="margin-left:auto;margin-right:auto;display:block;width:50%;">
-		{{-- <center><h1 style="font-family:'Nunito', sans-serif;color: #636b6f;font-weight: 200;height: 100vh;margin: 0;">DRAGON BALL</h1></center> --}}
+<body>
+	<div>
+		<img class="sl" src="images/team.png" style="width:50%; position:fixed">	  
+		<img class="sl" src="images/naga.png" style="width:50%; position:fixed; padding-top:350px">
+		<img src="images/api.gif" alt="Api" style="width:100%;padding-top:500px;position:fixed">
 	</div>
-	<img src="images/api.gif" alt="Api" style="width:100%">
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,65 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{!! asset('css/style.css') !!}" media="screen">
 
         <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 20px;
-                font-size: 15px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -86,7 +33,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="{{ URL('/app') }}" class="btn btn-sm btn-raised btn-info">NEXT</a>
+                        <a href="{{ URL('/app') }}"><button>NEXT</button></a>
                 </div>
             </div>
         </div>
